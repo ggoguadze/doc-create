d2f07d926b69f948edaa137c331fc637b5b23d17
Gvido Goguadze <gvidogoguadze@gmail.com>
2022-12-11 12:20:51 +0200

    invoice format adjustments

--- pages/invoice/[invoiceId].tsx
+++ pages/invoice/[invoiceId].tsx
@@ -2,17 +2,11 @@ import { Divider } from 'primereact/divider';
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 
-const products = [
-    { "id": "1000", "code": "f230fh0g3", "name": "Bamboo Watch", "description": "Product Description", "image": "bamboo-watch.jpg", "price": 65, "category": "Accessories", "quantity": 24, "inventoryStatus": "INSTOCK", "rating": 5 },
-    { "id": "1001", "code": "nvklal433", "name": "Black Watch", "description": "Product Description", "image": "black-watch.jpg", "price": 72, "category": "Accessories", "quantity": 61, "inventoryStatus": "INSTOCK", "rating": 4 },
-    { "id": "1002", "code": "zz21cz3c1", "name": "Blue Band", "description": "Product Description", "image": "blue-band.jpg", "price": 79, "category": "Fitness", "quantity": 2, "inventoryStatus": "LOWSTOCK", "rating": 3 },
-    { "id": "1003", "code": "244wgerg2", "name": "Blue T-Shirt", "description": "Product Description", "image": "blue-t-shirt.jpg", "price": 29, "category": "Clothing", "quantity": 25, "inventoryStatus": "INSTOCK", "rating": 5 },
-    { "id": "1004", "code": "h456wer53", "name": "Bracelet", "description": "Product Description", "image": "bracelet.jpg", "price": 15, "category": "Accessories", "quantity": 73, "inventoryStatus": "INSTOCK", "rating": 4 },
-    { "id": "1005", "code": "av2231fwg", "name": "Brown Purse", "description": "Product Description", "image": "brown-purse.jpg", "price": 120, "category": "Accessories", "quantity": 0, "inventoryStatus": "OUTOFSTOCK", "rating": 4 },
-    { "id": "1006", "code": "bib36pfvm", "name": "Chakra Bracelet", "description": "Product Description", "image": "chakra-bracelet.jpg", "price": 32, "category": "Accessories", "quantity": 5, "inventoryStatus": "LOWSTOCK", "rating": 3 },
-    { "id": "1007", "code": "mbvjkgip5", "name": "Galaxy Earrings", "description": "Product Description", "image": "galaxy-earrings.jpg", "price": 34, "category": "Accessories", "quantity": 23, "inventoryStatus": "INSTOCK", "rating": 5 },
-    { "id": "1008", "code": "vbb124btr", "name": "Game Controller", "description": "Product Description", "image": "game-controller.jpg", "price": 99, "category": "Electronics", "quantity": 2, "inventoryStatus": "LOWSTOCK", "rating": 4 },
-    { "id": "1009", "code": "cm230f032", "name": "Gaming Set", "description": "Product Description", "image": "gaming-set.jpg", "price": 299, "category": "Electronics", "quantity": 63, "inventoryStatus": "INSTOCK", "rating": 3 }
+
+const product = [
+    { "id": "1000", "name": "Saldais krējums", "price": 1.5, "unit": "kg", "quantity": 1 },
+    { "id": "1001", "name": "Skābais krējums", "price": 1.5, "unit": "gab", "quantity": 12 },
+    { "id": "1002", "name": "Kartupeļi", "price": 2.5, "unit": "kg", "quantity": 3 },
 ]
 function invoiceDetail() {
     return (
@@ -48,20 +42,30 @@ function invoiceDetail() {
                     <h6>Transporta līdzeklis:</h6>
                     <h6>Vadītājs:</h6>
                 </div>
-                <Divider />
+
                 <div className='invoice-items'>
-                    <DataTable value={products} responsiveLayout="scroll">
-                        <Column field="code" header="Code"></Column>
-                        <Column field="name" header="Name"></Column>
-                        <Column field="category" header="Category"></Column>
-                        <Column field="quantity" header="Quantity"></Column>
+                    <DataTable value={product} responsiveLayout="scroll">
+                        <Column field="name" header="Preču nosaukums"></Column>
+                        <Column body={(product) => {
+                            return product.quantity + ' ' + product.unit;
+                        }} field="quantity" header="Daudzums"></Column>
+                        <Column field="price" header="Cena"></Column>
+                        <Column body={(product) => {
+                            return product.quantity * product.price;
+                        }} header="Summa"></Column>
                     </DataTable>
                 </div>
-                <Divider />
+
                 <div className='invoice-total'>
-                    <h6>Kopā:</h6>
-                    <h6>PVN 21%:</h6>
-                    <h6>Summa apmaksai:</h6>
+                    <h6>Kopā: {
+                        product.reduce((total, product) => total + product.quantity * product.price, 0).toFixed(2)
+                    } Eur</h6>
+                    <h6>PVN 21%: {
+                        (product.reduce((total, product) => total + product.quantity * product.price, 0) * 0.21).toFixed(2)
+                    } Eur</h6>
+                    <h6>Summa apmaksai: {
+                        (product.reduce((total, product) => total + product.quantity * product.price, 0) * 1.21).toFixed(2)
+                    } Eur</h6>
                 </div>
             </div>
             <Divider />
